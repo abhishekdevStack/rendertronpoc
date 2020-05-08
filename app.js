6760e90496e620fdf6dde3781e162d535d1b9a8c
at <saurabh1512@live.com>
2020-05-08 20:55:37 +0530

    Trigger Heroku deploy after enabling collectstatic

--- app.js
+++ app.js
@@ -1,43 +1,89 @@
-var express = require("express");
-var path = require("path");
-var fs = require("fs");
-const rendertron = require("rendertron-middleware");
+const express = require("express");
+const fetch = require("node-fetch");
+const url = require("url");
+const app = express();
+const path = require("path");
+const appUrl = "arcane-ridge-37333.herokuapp.com";
+const renderUrl = "https://crwn-db-2a4e6.uc.r.appspot.com/render";
+app.use("/", express.static(__dirname + "/../dist"));
 
-var app = express();
+function generateUrl(request) {
+  return url.format({
+    protocol: request.protocol,
+    host: appUrl,
+    pathname: request.originalUrl,
+  });
+}
 
-const bots = [
-  "baiduspider",
-  "bingbot",
-  "embedly",
-  "facebookexternalhit",
-  "linkedinbot",
-  "outbrain",
-  "pinterest",
-  "quora link preview",
-  "rogerbot",
-  "showyoubot",
-  "slackbot",
-  "twitterbot",
-  "vkShare",
-  "W3C_Validator",
-  "whatsapp",
-  "mozilla/5.0 (x11; linux x86_64) applewebkit/537.36 (khtml, like gecko) headlesschrome/79.0.3945.0 safari/537.36",
-  "mozilla",
-  "applewebkit",
-  "headlesschrome",
-];
-const BOTS = rendertron.botUserAgents.concat(bots.join(","));
-const BOT_UA_PATTERN = new RegExp(BOTS.join("|"), "i");
-app.use(
-  rendertron.makeMiddleware({
-    proxyUrl: "https://crwn-db-2a4e6.uc.r.appspot.com/render",
-    userAgentPattern: BOT_UA_PATTERN,
-  })
-);
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/dist/my-first-app"));
+function detectBot(userAgent) {
+  console.log("detect bot function called");
+  // List of bots to target, add more if you'd like
+  const bots = [
+    // crawler bots
+    "googlebot",
+    "bingbot",
+    "yandexbot",
+    "duckduckbot",
+    "slurp",
+    // link bots
+    "twitterbot",
+    "facebookexternalhit",
+    "linkedinbot",
+    "embedly",
+    "baiduspider",
+    "pinterest",
+    "slackbot",
+    "vkShare",
+    "facebot",
+    "outbrain",
+    "W3C_Validator",
+    "mozilla/5.0 (x11; linux x86_64) applewebkit/537.36 (khtml, like gecko) headlesschrome/79.0.3945.0 safari/537.36",
+    "mozilla",
+    "applewebkit",
+    "headlesschrome",
+  ];
+  const agent = userAgent.toLowerCase();
+  for (const bot of bots) {
+    if (agent.indexOf(bot) > -1) {
+      console.log("bot detected", bot, agent);
+      return true;
+    } else {
+      console.log("sorry !bot not detected");
+    }
+  }
+  console.log("no bots found");
+  return false;
+}
+
+app.get("*", (req, res) => {
+  const isBot = detectBot(req.headers["user-agent"]);
+  if (isBot) {
+    const botUrl = generateUrl(req);
+    // If Bot, fetch url via rendertron
+    fetch(`${renderUrl}/${botUrl}`)
+      .then((res) => res.text())
+      .then((body) => {
+        // Set the Vary header to cache the user agent, based on code from:
+        // https://github.com/justinribeiro/pwa-firebase-functions-botrender
+        res.set("Cache-Control", "public, max-age=300, s-maxage=600");
+        res.set("Vary", "User-Agent");
+        console.log(`url-format-${renderUrl}/${botUrl}`);
+        console.log(`bot-url-${botUrl}`);
+        console.log("body->", body.toString());
+        res.send(body.toString());
+      });
+  } else {
+    console.log("you are a human!");
+    res.sendFile(path.join(__dirname + "/dist", "index.html"));
+  }
+});
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
-app.get("*", express.static(path.join(__dirname, "/dist/my-first-app")));
-
+app.listen(8000, function () {
+  console.log("Vola!! The app is listening on port 8000!");
+});
 module.exports = app;
