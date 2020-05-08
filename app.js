87d117a9f84097c8ef0b89f9567f0fd88214256a
at <saurabh1512@live.com>
2020-05-08 19:23:20 +0530

    Trigger Heroku deploy after enabling collectstatic

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ function detectBot(userAgent) {
     "vkShare",
     "W3C_Validator",
     "whatsapp",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36",
   ];
   console.log(userAgent);
   const agent = userAgent.toLowerCase();
