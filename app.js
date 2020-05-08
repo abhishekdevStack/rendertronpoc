9d99a41766b93b97c4fadc2719938937b79bb16e
at <saurabh1512@live.com>
2020-05-08 14:54:05 +0530

    Trigger Heroku deploy after enabling collectstatic

--- app.js
+++ app.js
@@ -22,12 +22,25 @@ const bots = [
   "W3C_Validator",
   "whatsapp",
 ];
-
+app.set("view engine", "ejs");
 app.use(
   rendertron.makeMiddleware({
     proxyUrl: "https://crwn-db-2a4e6.uc.r.appspot.com/render",
   })
 );
+// Set 'views' directory for any views
+// being rendered res.render()
+app.set("views", path.join(__dirname, "views"));
+console.log(path.join(__dirname, "views"));
+app.get("/testing", function (req, res) {
+  let metaTagInfo = [
+    {
+      image:
+        "https://images.pexels.com/photos/47367/full-moon-moon-bright-sky-47367.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+  ];
+  res.render("pages/testing.ejs", { metaTagInfo: metaTagInfo });
+});
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname));
 });
