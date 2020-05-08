bf62baf1645520859f2f1770a96c584adec70082
at <saurabh1512@live.com>
2020-05-08 19:08:27 +0530

    Trigger Heroku deploy after enabling collectstatic

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const bots = [
   "vkShare",
   "W3C_Validator",
   "whatsapp",
+  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/79.0.3945.0 Safari/537.36",
 ];
 // app.set("view engine", "ejs");
 app.use(
