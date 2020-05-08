d8bb719133eb3520c6651bac059820474c540102
at <saurabh1512@live.com>
2020-05-08 10:31:41 +0530

    Trigger Heroku deploy after enabling collectstatic

--- app.js
+++ app.js
@@ -2,35 +2,69 @@ var express = require("express");
 var path = require("path");
 var fs = require("fs");
 const rendertron = require("rendertron-middleware");
-
+const url = require("url");
 var app = express();
+const fetch = require("node-fetch");
 
-const bots = [
-  "baiduspider",
-  "bingbot",
-  "embedly",
-  "facebookexternalhit",
-  "linkedinbot",
-  "outbrain",
-  "pinterest",
-  "quora link preview",
-  "rogerbot",
-  "showyoubot",
-  "slackbot",
-  "twitterbot",
-  "vkShare",
-  "W3C_Validator",
-  "whatsapp",
-];
+// app.use(
+//   rendertron.makeMiddleware({
+//     proxyUrl: "https://crwn-db-2a4e6.uc.r.appspot.com/",
+//     userAgentPattern: new RegExp(bots.join("|"), "i"),
+//   })
+// );
+const appUrl = "arcane-ridge-37333.herokuapp.com";
+const renderUrl = "https://crwn-db-2a4e6.uc.r.appspot.com/render";
 
-app.use(
-  rendertron.makeMiddleware({
-    proxyUrl: "https://crwn-db-2a4e6.uc.r.appspot.com/render",
-    userAgentPattern: new RegExp(bots.join("|"), "i"),
-  })
-);
+function generateUrl(request) {
+  return url.format({
+    protocol: request.protocol,
+    host: appUrl,
+    pathname: request.originalUrl,
+  });
+}
+function detectBot(userAgent) {
+  const bots = [
+    "baiduspider",
+    "bingbot",
+    "embedly",
+    "facebookexternalhit",
+    "linkedinbot",
+    "outbrain",
+    "pinterest",
+    "quora link preview",
+    "rogerbot",
+    "showyoubot",
+    "slackbot",
+    "twitterbot",
+    "vkShare",
+    "W3C_Validator",
+    "whatsapp",
+  ];
+  const agent = userAgent.toLowerCase();
+  for (const bot of bots) {
+    if (agent.indexOf(bot) > -1) {
+      console.log("bot detected", bot.agent);
+      return true;
+    }
+  }
+  console.log("no bots detected");
+  return false;
+}
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname));
+  //   res.sendFile(path.join(__dirname));
+  const isBot = detectBot(req.headers["user-agent"]);
+  if (isBot) {
+    const botUrl = generateUrl(req);
+    console.log("botUrl", botUrl);
+    fetch(`${renderUrl}/${botUrl}`)
+      .then((res) => res.text())
+      .then((body) => {
+        console.log("body", body.toString());
+        res.send(body.toString());
+      });
+  } else {
+    res.sendFile(path.join(__dirname));
+  }
 });
 
 app.use(express.static(path.join(__dirname, "/dist/my-first-app")));
