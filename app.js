797ad91ea2817d6a75266e6b428ea5908b65cf99
at <saurabh1512@live.com>
2020-05-08 20:37:56 +0530

    Trigger Heroku deploy after enabling collectstatic

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(
   })
 );
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname));
+  res.sendFile(path.join(__dirname, "/dist/my-first-app/index.html"));
 });
 
 app.get("*", express.static(path.join(__dirname, "/dist/my-first-app")));
