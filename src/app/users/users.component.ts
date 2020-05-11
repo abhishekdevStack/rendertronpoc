4930fb88e3afbade82d552c77b94c54ffc0bf566
at <abhishek.srm@outlook.com>
2020-05-11 18:53:24 +0530

    Trigger Heroku deploy after enabling collectstatic

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,23 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { SeoService } from "src/seoService";
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  selector: "app-users",
+  templateUrl: "./users.component.html",
+  styleUrls: ["./users.component.css"],
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  constructor(private seo: SeoService) {}
   users = [
     {
       id: 1,
-      name: 'Max'
+      name: "Max",
     },
     {
       id: 2,
-      name: 'Anna'
+      name: "Anna",
     },
     {
       id: 3,
-      name: 'Chris'
-    }
+      name: "Chris",
+    },
   ];
+
+  ngOnInit() {
+    this.seo.generateTags({
+      title: "Users Page",
+      description: "Find All detail about users",
+      image:
+        "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      slug: "servers-page",
+      url: "https://arcane-ridge-37333.herokuapp.com/users",
+    });
+  }
 }
